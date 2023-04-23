bced8ec810396e39b094d243d0da89c11da74cc2
heraden <127416579+heraden@users.noreply.github.com>
2023-04-23 21:51:23 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
- function savingInformation() { 
-     const customerName = prompt("Enter name"); 
-    const customerAge = prompt("Enter age"); 
-     const ordersNumber = prompt("Enter number of order"); 
-    const ordersAmount  = prompt("Enter amount of order"); 
-    if (ordersAmount>=100 && ordersNumber>=4 && customerAge>60 ) { 
-         console.log("Receives a discount") 
-     }else{ 
-        console.log("Do not receive a discount") 
-     } 
- } 
- savingInformation(); 
+ function check(customer) {
+     const { name, age, numOrders, orderAmounts } = customer;
+     const total = orderAmounts.reduce((acc, cur) => acc + cur, 0);
+  
+    if (age >= 60 || (numOrders >= 4 && total >= 100)) {
+      return ${name}true;
+     } else {       return ${name}  false ;
+    }
+   }
+   const customer = {
+     name: "Turkan",
+     age: 65,
+     numOrders: 5,
+     orderAmounts: [30, 20, 40, 10, 25]
+   };
+  
+   console.log(check(customer));
